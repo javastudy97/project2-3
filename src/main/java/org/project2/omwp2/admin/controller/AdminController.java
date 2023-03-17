6bddd5e8ffb772751183787ae99fe395f2676386
javastudy97 <kimhr2020@gmail.com>
2023-03-17 13:03:19 +0900

    관리자페이지 검토

---
+++ src/main/java/org/project2/omwp2/admin/controller/AdminController.java
@@ -0,0 +1,21 @@
+package org.spring.footballAdmin.contorller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    @GetMapping({"","/index"})
+    public String Go(){
+
+       return "adminPage";
+    }
+
+    @GetMapping("/board")
+    public String boardPage(){
+        return "boardPage";
+    }
+}
