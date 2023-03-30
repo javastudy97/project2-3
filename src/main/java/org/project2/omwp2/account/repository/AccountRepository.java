ab8da843e606bd538ed17ed26a891dc67c4d1549
vo00ov <tookaing1324@gmail.com>
2023-03-30 12:29:06 +0900

    잔여금

--- src/main/java/org/project2/omwp2/account/repository/AccountRepository.java
+++ src/main/java/org/project2/omwp2/account/repository/AccountRepository.java
@@ -2,9 +2,14 @@ package org.project2.omwp2.account.repository;
 
 import org.project2.omwp2.entity.AccountEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AccountRepository extends JpaRepository<AccountEntity, Long> {
 
+    /* 현재 잔여금 */
+    @Query(value = "SELECT SUM(ac_income) - SUM(ac_expend) FROM account WHERE ac_id <=:ac_id", nativeQuery = true)
+    int findByAcId(@Param("ac_id") Long acId);
 }
